0148ddf320f06b53bf5547882b49e316f2dcec93
Leonardo Perez <find.leonardo@gmail.com>
2016-09-24 03:48:03 -0400

    Added Administrator class.

---
+++ src/models/Administrator.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by Leonardo on 9/24/2016.
+ */
+public class Administrator extends User {
+
+    public Administrator(String firstName, String lastName, String email, String phone) {
+        super(firstName, lastName, email, phone);
+    }
+}
